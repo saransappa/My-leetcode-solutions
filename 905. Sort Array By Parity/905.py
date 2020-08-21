3b1ea836ca2295086150365faca194ce2a161fea
Saran Sappa <saran.sappa@gmail.com>
2020-08-21 16:57:07 +0530

    added problem 905

---
+++ 905. Sort Array By Parity/905.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        e = []
+        o = []
+        for i in A:
+            if i%2==0:
+                e.append(i)
+            else:
+                o.append(i)
+        e.extend(o)
+        return e
