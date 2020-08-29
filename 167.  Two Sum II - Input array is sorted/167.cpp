cb0468a09fc7c2813c44807a4562a8363129eceb
Saran Sappa <saran.sappa@gmail.com>
2020-08-29 19:55:25 +0530

    added problem 167

---
+++ 167.  Two Sum II - Input array is sorted/167.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0,j=numbers.size()-1;
+        vector<int> v;
+        while(i<=j){
+            if(numbers[i]+numbers[j]==target){
+                v.push_back(i+1);
+                v.push_back(j+1);
+                break;
+            }
+            else if(numbers[i]+numbers[j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return v;
+    }
+};
